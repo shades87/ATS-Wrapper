4161b34443c2c788f947b1bb6545a6dfa9d25339
Daniel McFadyen <shadyshadyshades@hotmail.com>
2024-06-29 18:53:49 +1000

    "Working on demographics"

--- demographicsBackUp.py
+++ demographicsBackUp.py
@@ -1,12 +1,36 @@
 #While creating my BERT model this is a call to chat GPT 3.5 to have functionality to the web-ui
 
 from openai import OpenAI
-client = OpenAI()
 
 #create a function to change demographics into a string
 #default for arguments is 0
 def demographics(age=0, ed=0, income=0, region=0, country=0):
       demo = ""
+      match age:
+         case 1:
+            demo = demo + "User is under 15. "
+
+         case 2:
+            demo = demo + "User is between 15 and 35. "
+
+         case 3:
+            demo = demo + "User is between 35 and 65. "
+
+         case 4:
+            demo = demo + "User is over 65. "
+
+      match ed:
+         case 1:
+            demo = demo + "User has graduated high school. "
+          
+         case 2:
+            demo = demo + "User has a Bachelor's degree. "
+
+         case 3:
+            demo = demo + "User has a PHD. "
+            
+
+        
 
 #create a function that can grab text from a website
 def get_article_text(article):
@@ -15,6 +39,7 @@ def get_article_text(article):
 #create a function to call chatgpt 3.5 using the demographics
 
 def summarise(demo, cont):
+  client = OpenAI()
   completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
