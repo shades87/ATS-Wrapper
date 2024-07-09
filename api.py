80b3f6f0ae4a9657158ba9902165bb033e359167
Daniel McFadyen <shadyshadyshades@hotmail.com>
2024-07-09 19:48:33 +1000

    "building api"

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from demographicsBackUp import *
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,3 +8,16 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Message": "Congrats! This is your first API!"}
+
+class summary(BaseModel):
+    url: str
+    age: int
+    nat: int
+    income: int
+    ed: int
+    city: int
+
+@app.post("/summarise")
+async def summarise():
+    
+    return summary
